cc56907e047362c17b03c6cac47cc5b3ebc99d73
Arnold <mutwirinold2@gmail.com>
2025-11-23 22:10:00 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #g++ ./src/main.cpp src/glad.c -Iinclude -lglfw -ldl -o myapp
+BUILD_DIRECTORY="./build"
+
 if [ $1 = "build" ]
 then
+    if [ ! -d "$BUILD_DIRECTORY" ]
+    then
+        mkdir build
+    fi
     cd build
     cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
     make
